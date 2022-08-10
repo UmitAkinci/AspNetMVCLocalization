abb25898b5a22181ee0b5f9dadc660a78a36a0e7
Ümit Akıncı <umit.akinci@yaz.com.tr>
2022-08-10 08:27:48 +0300

    added homeview model

---
+++ AspNetMVCLocalization/AspNetMVCLocalization/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetMVCLocalization.Models
+{
+    [BindProperties()]
+    public class HomeViewModel
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
